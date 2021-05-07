2a8dee03e23c9faefaad45055cada87802ee8053
Laila Maia <lailamaiacr01@gmail.com>
2021-05-07 19:12:47 -0300

    adicionando

---
+++ Pessoa.java
@@ -0,0 +1,16 @@
+class Pessoa{
+    private String nome;
+    private int matricula;
+    private String sexo;
+
+    public String getNome() {
+        return this.nome;
+        
+    }
+    public void SetNome(nome){
+        this.nome = nome;
+    }
+    public int calcularMatricula(matricula){
+        return matricula *10;
+    }
+}
